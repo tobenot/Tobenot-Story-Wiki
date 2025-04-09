f633e4be727a159effa534e56fbe634245900c27
tobenot <42083410+tobenot@users.noreply.github.com>
2025-04-10 03:35:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Tobenot-Story-Wiki/', // 修改为仓库名，用于 GitHub Pages 部署
+  base: '/Tobenot-Story-Wiki/', // 修改为仓库名，用于 GitHub Pages 部署  
   resolve: {
     alias: {
       '@': './src'

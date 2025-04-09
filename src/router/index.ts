7366feb36cb8a37083b0d640f7a0614e745c95d1
tobenot <42083410+tobenot@users.noreply.github.com>
2025-04-10 03:30:54 +0800

    build

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory('/Tobenot-Story-Wiki/'),
   routes
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   base: '/Tobenot-Story-Wiki/', // 修改为仓库名，用于 GitHub Pages 部署
   resolve: {
     alias: {
-      '@': '/src'
+      '@': './src'
     }
   }
 })
